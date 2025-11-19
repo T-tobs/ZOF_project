dbe6700b29a98ac8b1e263b775ae246697f4a5c5
T-tobs <tokeke.2202836@stu.cu.edu.ng>
2025-11-19 10:46:44 +0100

    added plotting capability with matplotlib

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 
 # ----------------------------------------------------
 #  Utility: Build polynomial function
